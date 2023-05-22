434882c10c9cf6ee80f2c1366dafae75e762ce3c
Manuel Gallego <diaferdesign@gmail.com>
2023-05-22 19:47:24 +0200

    Subida inicial de archivos

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import './mystyles.scss';
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+<h2>Este fragmento de código ha sido generado con React</h2>
+);
